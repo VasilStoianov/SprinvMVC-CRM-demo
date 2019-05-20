d5f1ebb32a4e2bc925456ea5fbead4915624a552
VasilStoianov <46459607+VasilStoianov@users.noreply.github.com>
2019-05-20 13:38:56 +0300

    Add files via upload
    Added Delete Link and Search option

--- web-customer-tracker/src/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/springdemo/dao/CustomerDAOImpl.java
@@ -22,7 +22,7 @@ public class CustomerDAOImpl implements CustomerDAO{
 	
 		Session session = sessionFactory.getCurrentSession();
 		
-		Query<Customer> allcustomers = session.createQuery("from Customer order by lastName", Customer.class);
+		Query<Customer> allcustomers = session.createQuery("from Customer", Customer.class);
 		
 		List<Customer> customers = allcustomers.getResultList();
 		
@@ -34,7 +34,7 @@ public class CustomerDAOImpl implements CustomerDAO{
 	public void saveCustomer(Customer theCustomer) {
 		Session session = sessionFactory.getCurrentSession();
 		
-		session.save(theCustomer);
+		session.saveOrUpdate(theCustomer);
 		
 	}
 
@@ -47,6 +47,40 @@ public class CustomerDAOImpl implements CustomerDAO{
 		Customer theCustomer = session.get(Customer.class, theId);
 		return theCustomer;
 		
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+		Session session = sessionFactory.getCurrentSession();
+		Query theQuery = session.createQuery("delete from Customer where id = " + theId);
+		theQuery.executeUpdate();
+		
+		
+	}
+
+	@Override
+	public List<Customer> searchCustomers(String theSearchName) {
+		Session session = sessionFactory.getCurrentSession();
+		
+		Query theQuery = null;
+		 if (theSearchName != null && theSearchName.trim().length() > 0) {
+
+	            // search for firstName or lastName ... case insensitive
+	            theQuery = session.createQuery("from Customer where lower(firstName) like '%"+theSearchName.toLowerCase()+"%' or lower(lastName) like '%"+theSearchName.toLowerCase()+"%'", Customer.class);
+//	            theQuery.setParameter("theName", "%" + theSearchName.toLowerCase() + "%");
+
+	        }
+	        else {
+	            // theSearchName is empty ... so just get all customers
+	            theQuery = session.createQuery("from Customer", Customer.class);            
+	        }
+	        
+	        // execute query and get result list
+	        List<Customer> customers = theQuery.getResultList();
+	                
+	        // return the results        
+	        return customers;
+	      
 	}
 	
 }
